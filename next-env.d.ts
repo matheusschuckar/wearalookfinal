949730eff60ffabaec8e92bec050e5a8502f6fc9
matheusschuckar <69393071+matheusschuckar@users.noreply.github.com>
2025-11-05 23:26:20 -0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,9 @@
 /// <reference types="next/image-types/global" />
 /// <reference types="next/navigation-types/compat/navigation" />
 
+// ADIÇÃO ESSENCIAL PARA FORÇAR O RECONHECIMENTO DE JSX NO BUILD
+/// <reference types="react/canary" />
+/// <reference types="react-dom/canary" />
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
