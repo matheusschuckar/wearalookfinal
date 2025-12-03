69e7605dd7e39ba65671d4869de6354d4f62507d
matheusschuckar <69393071+matheusschuckar@users.noreply.github.com>
2025-12-03 17:12:25 -0300

    Update page.tsx

--- app/parceiros/cupons/gerenciar/page.tsx
+++ app/parceiros/cupons/gerenciar/page.tsx
@@ -1,6 +1,9 @@
 // app/parceiros/cupons/gerenciar/page.tsx
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import ManageCouponsClient from "./client";
 
 export default function ManageCouponsPage() {
